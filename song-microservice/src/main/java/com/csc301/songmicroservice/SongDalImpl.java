ce0ac3d63f2b08edb508d901338b2759b5303753
Jimmy Yu <jimmyyu82@gmail.com>
2020-11-25 02:42:44 -0500

    Implement functionality to get song data by Id with proper error handling

--- song-microservice/src/main/java/com/csc301/songmicroservice/SongDalImpl.java
+++ song-microservice/src/main/java/com/csc301/songmicroservice/SongDalImpl.java
@@ -1,11 +1,19 @@
 package com.csc301.songmicroservice;
 
+import java.util.Iterator;
+
+import org.bson.Document;
 import org.bson.types.ObjectId;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoIterable;
 
 @Repository
 public class SongDalImpl implements SongDal {
@@ -25,8 +33,33 @@ public class SongDalImpl implements SongDal {
 
 	@Override
 	public DbQueryStatus findSongById(String songId) {
-		// TODO Auto-generated method stub
-		return null;
+		DbQueryStatus dbqs = null;
+
+		try {
+			// Create a query to find post by the given Id
+			BasicDBObject query = new BasicDBObject();
+			query.put("_id", new ObjectId(songId));
+			MongoIterable<Document> songs = db.getCollection("songs").find(query);
+
+			Iterator iterator = songs.iterator();
+			
+			// If the iterator is empty then the song could not be found
+			if(!iterator.hasNext()) {
+				return new DbQueryStatus("Error retrieving song. Song not found", DbQueryExecResult.QUERY_ERROR_NOT_FOUND);
+			}
+			
+			// Get the song with the given id and store its info in dbqs
+			while (iterator.hasNext()) {
+				Document song = (Document) iterator.next(); 
+				JSONObject jsonObj = new JSONObject(song.toJson()); 
+				dbqs = new DbQueryStatus("Retrieving song", DbQueryExecResult.QUERY_OK);
+				dbqs.setData(jsonObj.toString());
+			}
+		} catch (Exception e) {
+			return new DbQueryStatus("Error retrieving song", DbQueryExecResult.QUERY_ERROR_GENERIC);
+		}
+
+		return dbqs;
 	}
 
 	@Override
